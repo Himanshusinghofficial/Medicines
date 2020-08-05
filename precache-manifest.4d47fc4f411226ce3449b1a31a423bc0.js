2d6bdd4371ac9ec43b55d2e2b193a9b38d752bd7
Himanshusinghofficial <hs882677@gmail.com>
2020-08-06 02:12:04 +0530

    Updates

---
+++ precache-manifest.4d47fc4f411226ce3449b1a31a423bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b609f5b1e897b31158166b7d5c08cfd",
+    "url": "/Medicines/index.html"
+  },
+  {
+    "revision": "860f4442a6afe0752806",
+    "url": "/Medicines/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "4ad204714472beace4d8",
+    "url": "/Medicines/static/js/2.9c9c6305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Medicines/static/js/2.9c9c6305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860f4442a6afe0752806",
+    "url": "/Medicines/static/js/main.7f016ec6.chunk.js"
+  },
+  {
+    "revision": "0da8c03edc12924f6e37",
+    "url": "/Medicines/static/js/runtime-main.e13fee4e.js"
+  }
+]);
